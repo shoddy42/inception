562d35aeea12835f06e4585f2b6ee8a5a3eab692
Wouter Konings <wkonings@f1r3s16.codam.nl>
2024-11-21 14:26:16 +0100

    removed pointless commented out legacy code

--- srcs/requirements/wordpress/tools/setup_wordpress.sh
+++ srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -9,23 +9,24 @@ done
 
 sed -i "s/listen = 127.0.0.1:9000/listen = 9000/g" /etc/php81/php-fpm.d/www.conf
 
-# rm /var/www/wordpress/wp-config.php
 wp config create --allow-root \
 	--dbname=${DB_NAME} \
 	--dbuser=${DB_USER} \
 	--dbpass=${DB_PASS} \
 	--dbhost=${DB_HOST} \
 	--force
-#	--url=${DOMAIN_NAME} \
+
 wp core install --allow-root \
 	--title=${WP_TITLE} \
 	--url=${DOMAIN_NAME} \
 	--admin_user=${WP_ADMIN_USER} \
 	--admin_password=${WP_ADMIN_PASS} \
 	--admin_email=${WP_ADMIN_EMAIL}
+
 wp user create ${WP_USER_NAME} ${WP_USER_EMAIL} \
 	--user_pass=${WP_USER_PASS} \
 	--role=${WP_USER_ROLE}
+
 wp option update home ${DOMAIN_NAME} --allow-root
 wp option update siteurl ${DOMAIN_NAME} --allow-root
 
